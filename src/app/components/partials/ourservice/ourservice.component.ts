3720bd1298c78c02b3775add2c45041a0c4117f9
Bosco Basabana <basabanansita@gmail.com>
2019-11-14 19:37:55 -0800

    stacks full

--- src/app/components/partials/ourservice/ourservice.component.ts
+++ src/app/components/partials/ourservice/ourservice.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { timingSafeEqual } from 'crypto';
 
 @Component({
   selector: 'app-ourservice',
@@ -6,8 +7,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./ourservice.component.scss']
 })
 export class OurserviceComponent implements OnInit {
+  showFrontendStacks: boolean;
+  showInfraStacks: boolean;
+  showBackendStacks:boolean;
+  showDatabaseStacks:boolean;
 
-  constructor() { }
+  constructor() { 
+    this.showFrontendStacks = false;
+    this.showInfraStacks = false;
+    this.showBackendStacks = false;
+    this.showDatabaseStacks = false;
+  }
 
   ngOnInit() {
   }
