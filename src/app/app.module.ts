60130f55b009c98ccdb7a954d8e0ef5f2a123efd
Bosco Basabana <basabanansita@gmail.com>
2019-11-24 11:12:45 +0100

    form manage

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -39,7 +40,8 @@ import { ScrollTopComponent } from './components/scroll-top/scroll-top.component
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/partials/contact/contact.component.ts
+++ src/app/components/partials/contact/contact.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
@@ -6,10 +7,37 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./contact.component.scss']
 })
 export class ContactComponent implements OnInit {
+  contactForm: FormGroup;
+  submitted     = false;
+  loadingSubmit = false;
+  TextBtnSignup = true;
 
-  constructor() { }
+  // pattern 
+  fullnamePattern = '^[a-z0-9_-]{8,15}$';
+  phonePattern = '^[a-z0-9_-]{8,15}$';
+
+  constructor( private formBuilder: FormBuilder) { }
 
   ngOnInit() {
+    this.contactForm = this.formBuilder.group({
+      fullname: ['', [Validators.required, Validators.pattern(this.fullnamePattern)]],
+      email: ['', [Validators.required, Validators.email]],
+      phone: ['', [Validators.required, Validators.pattern(this.phonePattern)]],
+      message: ['', [Validators.required, Validators.maxLength(256)]],
+    });
+  }
+
+  get bb() { return  this.contactForm.controls; }
+  onSubmit(){
+    this.submitted = true;
+    console.log(this.contactForm.value);
+    if (this.contactForm.invalid) {
+      alert('Please fill in the fields correctly');
+      return ;
+   }
+    this.loadingSubmit = true;
+    this.TextBtnSignup = false;
+    this.contactForm.reset();
   }
 
 }
